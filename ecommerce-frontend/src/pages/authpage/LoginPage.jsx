2be7030ae4a66c7b5024a2720ee7139bbdab1164
sankarrayi <rayisankar8888@gmail.com>
2025-08-07 21:46:54 +0530

    Add auth APIs functionality logic and form handling

--- ecommerce-frontend/src/pages/authpage/LoginPage.jsx
+++ ecommerce-frontend/src/pages/authpage/LoginPage.jsx
@@ -1,20 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+
 import Header from '../../components/Header';
 
 import './AuthPage.css';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleInput = (setter) => (e) => setter(e.target.value);
+
+  async function authLogin(event) {
+    event.preventDefault();
+
+    try {
+      await axios.post('/api/users/login', {
+        email,
+        password
+      }, {
+        withCredentials: true
+      });
+
+      navigate('/'); 
+
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      const message = error.response?.data?.message || 'Login failed';
+      setErrorMessage(message);
+    }
+  }
   return (
     <>
-      <Header/>
-      
-      <div className="auth-page">
+      <Header />
+
+      <div className="auth-page" >
         <h2>Login to Your Account</h2>
-        <form className="auth-form">
-          <input type="email" placeholder="Email address" required />
-          <input type="password" placeholder="Password" required />
+        <form className="auth-form" onSubmit={authLogin}>
+          <input type="email" placeholder="Email address"
+          value={email} onChange={handleInput(setEmail)} required />
+          <input type="password" placeholder="Password" 
+          value={password} onChange={handleInput(setPassword)}required />
           <button type="submit">Login</button>
         </form>
+        {errorMessage && <p className="error">{errorMessage}</p>}
         <p>
           Don't have an account? <Link to="/register">Register here</Link>
         </p>
--- ecommerce-frontend/src/pages/authpage/RegisterPage.jsx
+++ ecommerce-frontend/src/pages/authpage/RegisterPage.jsx
@@ -1,21 +1,85 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+
 import Header from '../../components/Header';
 
 import './AuthPage.css';
 
 export default function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  // input handlers
+  /*
+  function inputName(event) {
+    setName(event.target.value);
+  }
+  function inputEmail(event) {
+    setEmail(event.target.value);
+  }
+  function inputPassword(event) {
+    setPassword(event.target.value);
+  }
+  */
+
+  const handleInput = (setter) => (e) => setter(e.target.value);
+
+
+  async function authRegister(event) {
+    event.preventDefault();
+
+    try {
+      await axios.post('/api/users/register', {
+        name,
+        email,
+        password
+      }, {
+        withCredentials: true
+      });
+
+      // console.log('Logged in user after register:', userData);
+
+      navigate('/');
+
+      setName('');
+      setEmail('');
+      setPassword('');
+
+    } catch (error) {
+      const message = error.response?.data?.message || 'Something went wrong';
+      setErrorMessage(message);
+    }
+
+  }
+
   return (
     <>
-      <Header/>
-      
+      <Header />
+
       <div className="auth-page">
         <h2>Create an Account</h2>
-        <form className="auth-form">
-          <input type="text" placeholder="Full Name" required />
-          <input type="email" placeholder="Email address" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Register</button>
+        <form className="auth-form" onSubmit={authRegister}>
+          <input type="text" placeholder="Full Name"
+            // onChange={inputName} 
+            onChange={handleInput(setName)}
+            value={name} required />
+          <input type="email" placeholder="Email address"
+            // onChange={inputEmail} 
+            onChange={handleInput(setEmail)}
+            value={email} required />
+          <input type="password" placeholder="Password"
+            // onChange={inputPassword} 
+            onChange={handleInput(setPassword)}
+            value={password} required />
+          <button type="submit"
+           /* onClick={authRegitser} */>Register</button>
         </form>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <p>
           Already have an account? <Link to="/login">Login here</Link>
         </p>
