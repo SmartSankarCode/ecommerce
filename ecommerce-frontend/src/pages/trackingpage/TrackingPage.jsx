ac451c66b3025eaee23f1baef10a67708dc67f56
sankarrayi <rayisankar8888@gmail.com>
2025-08-13 22:25:19 +0530

    Add  orders api to get matching order and item in tracking

--- ecommerce-frontend/src/pages/trackingpage/TrackingPage.jsx
+++ ecommerce-frontend/src/pages/trackingpage/TrackingPage.jsx
@@ -1,45 +1,81 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+
 import Header from '../../components/Header';
 import './TrackingPage.css';
 
+
 export default function TrackingPage({ cartQuantity }) {
+  const { orderId, itemId } = useParams();
+  const [item, setItem] = useState(null);
+
+
+  useEffect(() => {
+    async function fetchOrderData() {
+      const res = await axios.get('/api/orders/my-orders', {
+        withCredentials: true
+      });
+
+      // find order by matching logic using id
+      const requiredOrder = res.data.find(order => {
+        return order._id === orderId
+      });
+
+      // find matching item
+      const requiredItem = requiredOrder.items.find(item => {
+        return item._id === itemId
+      });
+
+      setItem(requiredItem);
+
+    }
+
+    fetchOrderData();
+  }, [])
 
   return (
     <>
       <Header cartQuantity={cartQuantity} />
-      <div>
-        <div className="tracking-page">
-          <div className="order-tracking">
+      {/* getting item details take more time than actual
+      bcz of matching technique used above */}
+      {item && (
+        <div>
+          <div className="tracking-page">
+            <div className="order-tracking">
 
-            <div className="delivery-date">
-              Arriving on Thursday, August 14
-            </div>
+              <div className="delivery-date">
+                Arriving on: {dayjs(item.estimatedDeliveryTime).format('MMMM D')}
+              </div>
 
-            <div className="product-info">
-              Elephants Love Candle Holder With LED Candle
-            </div>
+              <div className="product-info">
+                {item.productId.name}
+              </div>
 
-            <div className="product-info">
-              Quantity: 1
-            </div>
+              <div className="product-info">
+                Quantity: {item.quantity}
+              </div>
 
-            <img
-              className="product-image"
-              src="/images/products/Elephants Love Candle Holder with LED Candle.jpg"
-              alt="Product"
-            />
+              <img
+                className="product-image"
+                src={`http://localhost:3000${item.productId.image}`}
+                alt={item.productId.name}
+              />
 
-            <div className="progress-labels-container">
-              <div className="progress-label">Preparing</div>
-              <div className="progress-label current-status">Shipped</div>
-              <div className="progress-label">Delivered</div>
-            </div>
+              <div className="progress-labels-container">
+                <div className="progress-label">Preparing</div>
+                <div className="progress-label current-status">Shipped</div>
+                <div className="progress-label">Delivered</div>
+              </div>
 
-            <div className="progress-bar-container">
-              <div className="progress-bar"></div>
+              <div className="progress-bar-container">
+                <div className="progress-bar"></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
